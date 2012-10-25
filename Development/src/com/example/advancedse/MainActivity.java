a2eb315255a81f86f9fdc9dfd65a68d0755e3b7d
ewencluley <ec237@sussex.ac.uk>
2012-10-25 11:08:51 +0100

    add code to save locations periodically to AWS db

--- Development/src/com/example/advancedse/MainActivity.java
+++ Development/src/com/example/advancedse/MainActivity.java
@@ -2,18 +2,18 @@ package com.example.advancedse;
 
 import java.util.Date;
 
-import android.location.GpsStatus.Listener;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 //import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -40,14 +40,33 @@ public class MainActivity extends Activity {
         	enableLocationSettings();
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, gpsUpdateListener);
+    
+        Thread timer = new Thread() {
+            public void run () {
+                for (;;) {
+                    // do stuff in a separate thread
+                	logGPSLocation.sendEmptyMessage(0);
+                    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}    // sleep for 3 seconds
+                }
+            }
+        };
+        timer.start();
     }
-
-    LocationListener gpsUpdateListener = new LocationListener(){
+    
+    private LocationData lastKnownLoc;
+    private LocationListener gpsUpdateListener = new LocationListener(){
     	public void onLocationChanged(Location location) {
             // A new location update is received.  Do something useful with it.  Update the UI with
             // the location update.
-            //updateUILocation(location);
-    		gpsText.setText("Latitude:"+location.getLatitude() + "\nLongitude:" + location.getLongitude());
+    		lastKnownLoc = new LocationData();
+    		lastKnownLoc.setLongitude(location.getLongitude());
+    		lastKnownLoc.setLatitude(location.getLatitude());
+    		lastKnownLoc.setTimestamp(new Date(location.getTime()));
+    		gpsText.setText("Latitude:"+ lastKnownLoc.getLatitude() + "\nLongitude:" + lastKnownLoc.getLongitude());
         }
 
         public void onProviderDisabled(String provider) {
@@ -70,4 +89,14 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    private Handler logGPSLocation = new Handler () {
+        public void handleMessage (Message msg) {
+            if(lastKnownLoc != null){
+            	System.out.println("starting save");
+            	lastKnownLoc.save();
+            	
+            }
+        }
+    };
 }
