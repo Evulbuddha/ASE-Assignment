d1a2faf259df95da1aa9d5c83a990866d50856e9
Ewen Cluley <ewencluley@hotmail.com>
2012-10-25 09:44:47 +0100

    add some test code for gps location, unfinished

--- TestAdvancedSETest/src/com/example/advancedse/test/GPSTest.java
+++ TestAdvancedSETest/src/com/example/advancedse/test/GPSTest.java
@@ -4,9 +4,12 @@ import com.example.advancedse.MainActivity;
 import com.example.advancedse.R;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
 public class GPSTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+	private TextView gpsLocationField;
+	
 	public GPSTest(String name) {
 		super("com.example.advancedse", MainActivity.class);
 	}
@@ -16,7 +19,9 @@ public class GPSTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	}
 	
 	public void testGPS(){
-		
+		MainActivity mainActivity = getActivity();  
+	    gpsLocationField = (TextView) mainActivity.findViewById(R.id.gpsLoc);
+	    
 	}
 
 }
