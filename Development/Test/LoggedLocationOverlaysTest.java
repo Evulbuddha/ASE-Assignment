587db55960c6541ed4b6a77c7642c093fa282f26
ao243 <andria.orphanidou@gmail.com>
2012-11-10 15:01:29 +0000

    commited

---
+++ Development/Test/LoggedLocationOverlaysTest.java
@@ -0,0 +1,143 @@
+package com.example.advancedse;
+
+import junit.framework.TestCase;
+
+public class LoggedLocationOverlaysTest extends TestCase {
+
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnTapInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testLoggedLocationOverlaysDrawable() {
+		fail("Not yet implemented");
+	}
+
+	public void testLoggedLocationOverlaysDrawableContext() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddOverlay() {
+		fail("Not yet implemented");
+	}
+
+	public void testCreateItemInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnTouchEvent() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnTrackballEvent() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnKeyUp() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnTapGeoPointMapView() {
+		fail("Not yet implemented");
+	}
+
+	public void testDrawCanvasMapViewBoolean() {
+		fail("Not yet implemented");
+	}
+
+	public void testItemizedOverlay() {
+		fail("Not yet implemented");
+	}
+
+	public void testBoundCenterBottom() {
+		fail("Not yet implemented");
+	}
+
+	public void testBoundCenter() {
+		fail("Not yet implemented");
+	}
+
+	public void testCreateItemInt1() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCenter() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetIndexToDraw() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetLatSpanE6() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetLonSpanE6() {
+		fail("Not yet implemented");
+	}
+
+	public void testPopulate() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetLastFocusedIndex() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetFocus() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetFocus() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetLastFocusedIndex() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetItem() {
+		fail("Not yet implemented");
+	}
+
+	public void testNextFocus() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnSnapToItem() {
+		fail("Not yet implemented");
+	}
+
+	public void testHitTest() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetOnFocusChangeListener() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetDrawFocusedItem() {
+		fail("Not yet implemented");
+	}
+
+	public void testOverlay() {
+		fail("Not yet implemented");
+	}
+
+	public void testDrawAt() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnKeyDown() {
+		fail("Not yet implemented");
+	}
+
+	public void testDrawCanvasMapViewBooleanLong() {
+		fail("Not yet implemented");
+	}
+
+}
